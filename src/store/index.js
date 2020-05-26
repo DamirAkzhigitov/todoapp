ee0cdcbfb7dcf1d5bf1165788382982e7d612c5f
DamirAkzhigitov <damir.ckelet@gmail.com>
2020-05-26 23:12:03 +0300

    now data is stored in local storage

--- src/store/index.js
+++ src/store/index.js
@@ -11,39 +11,14 @@ export default new Vuex.Store({
         SET_TODOGROUP(state, list) {
             state.todoGroupList = list
         },
-        ADD_NEWCARD(state, card) {
-            state.todoGroupList[card.id].results.push(card.body)
-        },
         ADD_TODOGROUP(state, newGroup) {
             state.todoGroupList.push(newGroup)
-        },
-        REMOVE_GROUP(state, id) {
-            state.todoGroupList = state.todoGroupList.filter(
-                (group, index) => index !== id
-            )
-        },
-        REMOVE_CARD(state, card) {
-            const data = state.todoGroupList
-            console.log('card = ', card)
-            console.log('data = ', data)
-            // const newGroupData = data[card.parentId].results.filter(
-            //     (value, index) => index !== card.id
-            // )
-            // console.log('newGroupData = ', newGroupData)
-            // console.log('data[card.parentId] = ', data[card.parentId])
-            // data[card.parentId] = newGroupData
-            // // state.todoGroupList = data
         }
     },
     actions: {
         getTodoState({ commit }) {
             const list = checkLocalStorage()
-            console.log('list = ', list)
             commit('SET_TODOGROUP', list)
-        },
-        writeTodoState({ commit }, data) {
-            localStorage.setItem('todoGroupList', JSON.stringify(data))
-            commit('SET_TODOGROUP', data)
         },
         removeTodoGroup({ commit }, id) {
             const newList = filterList(id)
@@ -73,7 +48,12 @@ export default new Vuex.Store({
             commit('ADD_TODOGROUP', newGroup)
         },
         removeTodoCard({ commit }, data) {
-            commit('REMOVE_CARD', data)
+            const newList = checkLocalStorage()
+            newList[data.parentId].results = newList[
+                data.parentId
+            ].results.filter((item, index) => index !== data.id)
+            localStorage.setItem('todoGroupList', JSON.stringify(newList))
+            commit('SET_TODOGROUP', newList)
         }
     },
     modules: {}
